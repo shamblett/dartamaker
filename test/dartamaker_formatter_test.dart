c06db756edbb476f8aefa0ec4dfcd07a7c2edadd
Steve Hamblett <steve.hamblett@linux.com>
2019-06-18 12:00:15 +0100

    Initial construction

--- test/dartamaker_formatter_test.dart
+++ test/dartamaker_formatter_test.dart
@@ -4,16 +4,17 @@
  * Date   : 11/06/2019
  * Copyright :  S.Hamblett
  */
-import 'dart:async';
-import 'dart:convert';
-import 'dart:io';
-import 'dart:math';
 
 import 'package:dartamaker/dartamaker.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('None', () {
+    final DartamakerFormatterNone none =
+        Dartamaker().formatter(DartamakerFormatterTypes.none);
+    const String str = 'theString';
+    expect(none.filter(5), '5');
+    expect(none.filter(str), str);
+    expect(none.postCommit(str), str);
   });
-
 }

88f5b3d5c41ee4dead86a2825c5bd4204a28ac9b
Steve Hamblett <steve.hamblett@linux.com>
2019-06-25 14:41:10 +0100

    First cut functionality

---
+++ example/dartamaker.dart
@@ -0,0 +1,12 @@
+/*
+ * Package : dartamaker
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 11/06/2019
+ * Copyright :  S.Hamblett
+ */
+
+// Full examples of usage are to be found in the README along with command line flags
+// tag meaning and usage etc.
+//
+// The templates directory contains two test templates, one for json, one for xml, again
+// taken from the README.
